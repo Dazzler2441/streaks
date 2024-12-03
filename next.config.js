5117a4bcb4a49525be88af0321c631e9b9a9428c
Happiest7 <zqiqttmiyd@use.startmail.com>
2024-12-03 14:10:27 +0100

    fixes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
-import withPWA from 'next-pwa';
-
 /** @type {import('next').NextConfig} */
-const config = withPWA({
+const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  // Disable PWA in development mode
+  disable: process.env.NODE_ENV === 'development',
 });
 
-export default config; 
+const nextConfig = {
+  webpack: (config) => {
+    config.infrastructureLogging = {
+      level: 'error',
+    };
+    return config;
+  },
+};
+
+module.exports = withPWA(nextConfig); 
